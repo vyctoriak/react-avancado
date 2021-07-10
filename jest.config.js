383f870088b82152e97e952f002d2bb7cb60f4bd
Vyctoria <vyctoria@Samuels-MacBook-Air.local>
2021-07-10 18:41:00 -0300

    explicacoes das configs do jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testEnvironment: 'jsdom', // pra dizer que tipo de ambiente teremos para testes e como mexeremos com broser, será o jsdom
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  testEnvironment: 'jsdom', // pra dizer que tipo de ambiente teremos para testes e como mexeremos com browser, será o jsdom
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], //pastas para o jest ignorar
+  collectCoverage: true, // para fazer teste de cobertura, se está cobrindo todas as funções, variáveis, etc
+  collectCoverageFrom: ['src/**/*.ts(x)'], //de onde coletar os arquivos para serem feitos os testes de cobertura
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] //carregar configurações desse arquivo antes de rodar os testes
 }
