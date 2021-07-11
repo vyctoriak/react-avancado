d598747478801f5cc7c8a3ee76486b9471a5efa8
Vyctoria <vyctoriak@gmail.com>
2021-07-10 21:53:14 -0300

    add RTL config and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('testing Main component', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument() //o i serve para NÃO fazer diferença entre maiúsculas ou minúsculas
+  })
+
+  it('should render correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
